17e1af62f569572e93df75aedab46d23fb6cefa1
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-05-22 22:03:56 +0200

    Update app.js

--- Deep-JS-Foundations, V3/app.js
+++ Deep-JS-Foundations, V3/app.js
@@ -208,3 +208,170 @@ console.log(isValidName("") === false);
 console.log(isValidName(" \t\n") === false);
 console.log(isValidName("X") === false);
 console.log(hoursAttended("", 6) === false);
+
+/ = = = = = = = = = = = = = = = = = = = = = = =
+// Double & Triple Equals
+
+// == checks value(loose)
+// === checks value and type (strict)
+
+console.log(1 == 1); // true
+console.log(1 === 1); // true
+
+console.log(1 == "1"); // true
+console.log(1 === "1"); // false
+
+// ! ! ! ! ! ! ! !
+
+var element = "25";
+
+var box1 = 16;
+var box2 = element.value;
+
+if (Number(box1) === Number(box2)) {
+  // ...
+}
+
+if (box1 == box2) {
+  // ...
+}
+
+// ! ! ! ! ! ! ! !
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// var workShopStudents = [];
+
+// if(workShopStudents){
+// if(Boolean(workShopStudents)){
+// if(true){
+//  Yep
+// }}}
+
+// if (workShopStudents == true){
+//   if("" == true){
+//     if(0 == 1){
+//       if(false){
+// Nope :(
+// }}}}
+
+// if (workShopStudents == false){
+// if("" == false){
+// if(0 == 0){
+// if(true){
+// Yep
+// }}}}
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// Equality exercises
+
+// TODO: write `findAll(..)`
+function findAll(match, arr) {
+  var ret = [];
+  for (let v of arr) {
+    if (Object.is(match, v)) {
+      ret.push(v);
+    } else if (match == null && v == null) {
+      ret.push(v);
+    } else if (typeof match == "boolean" && typeof v == "boolean") {
+      if (match == v) {
+        ret.push(v);
+      }
+    } else if (
+      typeof match == "string" &&
+      match.trim() != "" &&
+      typeof v == "number" &&
+      !Object.is(v, -0)
+    ) {
+      if (match == v) {
+        ret.push(v);
+      }
+    } else if (
+      typeof match == "number" &&
+      !Object.is(match, -0) &&
+      !Object.is(match, NaN) &&
+      !Object.is(match, Infinity) &&
+      !Object.is(match, -Infinity) &&
+      typeof v == "string" &&
+      v.trim() != ""
+    ) {
+      if (match == v) {
+        ret.push(v);
+      }
+    }
+  }
+  return ret;
+}
+
+// tests:
+var myObj = { a: 2 };
+
+var values = [
+  null,
+  undefined,
+  -0,
+  0,
+  13,
+  42,
+  NaN,
+  -Infinity,
+  Infinity,
+  "",
+  "0",
+  "42",
+  "42hello",
+  "true",
+  "NaN",
+  true,
+  false,
+  myObj,
+];
+
+console.log("Equality exercises:");
+console.log(setsMatch(findAll(null, values), [null, undefined]) === true);
+console.log(setsMatch(findAll(undefined, values), [null, undefined]) === true);
+console.log(setsMatch(findAll(0, values), [0, "0"]) === true);
+console.log(setsMatch(findAll(-0, values), [-0]) === true);
+console.log(setsMatch(findAll(13, values), [13]) === true);
+console.log(setsMatch(findAll(42, values), [42, "42"]) === true);
+console.log(setsMatch(findAll(NaN, values), [NaN]) === true);
+console.log(setsMatch(findAll(-Infinity, values), [-Infinity]) === true);
+console.log(setsMatch(findAll(Infinity, values), [Infinity]) === true);
+console.log(setsMatch(findAll("", values), [""]) === true);
+console.log(setsMatch(findAll("0", values), [0, "0"]) === true);
+console.log(setsMatch(findAll("42", values), [42, "42"]) === true);
+console.log(setsMatch(findAll("42hello", values), ["42hello"]) === true);
+console.log(setsMatch(findAll("true", values), ["true"]) === true);
+console.log(setsMatch(findAll(true, values), [true]) === true);
+console.log(setsMatch(findAll(false, values), [false]) === true);
+console.log(setsMatch(findAll(myObj, values), [myObj]) === true);
+
+console.log(setsMatch(findAll(null, values), [null, 0]) === false);
+console.log(setsMatch(findAll(undefined, values), [NaN, 0]) === false);
+console.log(setsMatch(findAll(0, values), [0, -0]) === false);
+console.log(setsMatch(findAll(42, values), [42, "42hello"]) === false);
+console.log(setsMatch(findAll(25, values), [25]) === false);
+console.log(
+  setsMatch(findAll(Infinity, values), [Infinity, -Infinity]) === false
+);
+console.log(setsMatch(findAll("", values), ["", 0]) === false);
+console.log(setsMatch(findAll("false", values), [false]) === false);
+console.log(setsMatch(findAll(true, values), [true, "true"]) === false);
+console.log(setsMatch(findAll(true, values), [true, 1]) === false);
+console.log(setsMatch(findAll(false, values), [false, 0]) === false);
+
+// ***************************
+
+function setsMatch(arr1, arr2) {
+  if (
+    Array.isArray(arr1) &&
+    Array.isArray(arr2) &&
+    arr1.length == arr2.length
+  ) {
+    for (let v of arr1) {
+      if (!arr2.includes(v)) return false;
+    }
+    return true;
+  }
+  return false;
+}
+
