f72087c2725d9796915e5bf381df803b254f5a0a
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-06-20 22:32:24 +0200

    Create alertAndTypeof.js

---
+++ Deep-JS-Foundations, V3/alertAndTypeof.js
@@ -0,0 +1,15 @@
+alert( Number("   123   ") ); // 123
+alert( Number("123z") );      // NaN (error reading a number at "z")
+alert( Number(true) );        // 1
+alert( Number(false) );       // 0
+
+alert( Boolean(1) ); // true
+alert( Boolean(0) ); // false
+
+alert( Boolean("hello") ); // true
+alert( Boolean("") ); // false
+
+alert( Boolean("0") ); // true
+alert( Boolean(" ") ); // spaces, also true (any non-empty string is true)
+
+// in progress...
